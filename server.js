d45fc384a51ad28ce57d61c96ec4b07247ded228
Pek1s <riku.pekis@gmail.com>
2017-11-23 13:10:24 +0200

    Setup node and react

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use(express.static('client/src'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+});
+
+app.listen(3002, function () {
+  console.log('server on port 3002')
+});
