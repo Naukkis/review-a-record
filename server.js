17c1762c4d274bf8c1d38448ed9d27adb20e055d
Naukkis <22678066+Naukkis@users.noreply.github.com>
2017-11-26 11:11:42 +0200

    add spotify api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const spotify = require('./spotify');
 
 const app = express();
 
@@ -14,10 +15,13 @@ app.use(morgan('dev'));
 
 app.use(express.static('client/src'));
 
+app.get('/searchArtist', spotify.searchArtist);
+app.get('/searchAlbum', spotify.searchAlbum);
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });
 
-app.listen(3002, function () {
+app.listen(3002,  function () {
   console.log('server on port 3002')
 });
