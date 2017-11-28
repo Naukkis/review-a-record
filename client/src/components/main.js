5fbc67b847c0fae6480a7103eaaa0daa35016941
Pek1s <riku.pekis@gmail.com>
2017-11-28 10:22:29 +0200

    add components folder

---
+++ client/src/components/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { store } from '../store';
+
+class Main extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default Main
