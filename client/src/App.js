bf1ed3d731418a8645981ef8ab2e798d6056e349
Pek1s <riku.pekis@gmail.com>
2017-11-30 11:54:43 +0200

    Add spotifytoken to client

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 import { searchAlbum, searchArtist, getAccessToken} from './spotify';
 import { Button } from 'react-bootstrap';
 import Main from './components/main.js';
-import Layout from './components/Layout.js'
+import Layout from './components/Layout.js';
+
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    getAccessToken();
+  }
   render() {
     return (
         <div className="Layout">
