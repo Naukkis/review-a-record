4f49905ecba6b2c68e069f2ea8c9622eafa89f28
Naukkis <22678066+Naukkis@users.noreply.github.com>
2017-12-03 20:17:41 +0200

    search now shows albums

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -6,9 +6,10 @@ class SearchResults extends React.Component {
 	render() {
 		return (
 			<div>
+			
 			{ store.getState().artists.items && 
-
-
+				<div>
+				<h2>Artists</h2>
 				<ul id="searchResults">
 				{ store.getState().artists.items.map((data) =>
 					<li key={data.id}>
@@ -19,6 +20,18 @@ class SearchResults extends React.Component {
 		            </li>
 				)}
 				</ul> 
+				<h2>Albums</h2>
+				<ul id="searchResults">
+				{ store.getState().artists.items.map((data) =>
+					<li key={data.id}>
+		               <p>{data.name}</p>
+		               { data.images.length > 0 && 
+		              	<img src={data.images[0].url} alt={data.name} />
+		               }
+		            </li>
+				)}
+				</ul> 
+				</div>
 				}
 			</div>
 		);
