ab1ba6e969efcccc65d756ad81509a6144805d2c
hanhav <33927775+hanhav@users.noreply.github.com>
2017-12-03 19:46:41 +0200

    CSS and home

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render(){
     return (
-      <footer>
-      <p>This is testing footer </p>
-      </footer>
+      <div id="f">
+	</div>
     );
   }
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Reviews extends Component {
+
+
+  render(){
+    return (
+      <div id="headerwrap">
+		<div className="container">
+			<br></br>
+			<div class="row">
+				<br></br>
+				<br></br>
+				<br></br>
+				<h1> Review App</h1>
+				<br></br>
+				<h3>Rewiew your favorite artists</h3>
+        <br></br>
+        <br></br>
+				<br></br>
+				<br></br>
+				<div class="col-lg-6 col-lg-offset-3">
+				</div>
+			</div>
+		</div>
+	</div>
+    );
+  }
+}
