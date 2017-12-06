753822377c3dfea4f0a71af8ffad7372f272f11f
hanhav <33927775+hanhav@users.noreply.github.com>
2017-12-06 18:43:40 +0200

    Review proto

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 
 export default class Reviews extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {name: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    e.target.value = '';
+    this.setState({ name: ''});
+  }
 
   render(){
       const mock = [{
         name: "diudiudiu",
+        review: "piupiupiupiu",
         time: "12:00"
       },
       {
@@ -39,9 +56,17 @@ export default class Reviews extends Component {
         {mock.map((x) =>
           <div className="review" key={x.name}>
             <p style={{color: "white" }}>{x.name}</p>
+            <p style={{color: "white" }}>{x.review}</p>
             <span className="time-right">{x.time}</span>
           </div>
         )}
+        <div>
+        <form className="search" onSubmit={this.handleSubmit}>
+				<input type="text" id="searchbox" placeholder="Search..." name={this.state.value}
+						onChange={this.handleChange} onSubmit={this.handleSubmit}>
+				</input>
+				</form>
+        </div>
         </div>
       </div>
     );
