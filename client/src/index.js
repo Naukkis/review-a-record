53e07b89abdc8aa2c332702fc11eff0a0c509882
hanhav <33927775+hanhav@users.noreply.github.com>
2017-12-09 18:06:13 +0200

    fix css and bootsrap conflict

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { store } from './store.js';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 let render = () => ReactDOM.render((
 	<BrowserRouter>
