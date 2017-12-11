30fc456418bbd26768db1725bbe42452bf12f649
hanhav <33927775+hanhav@users.noreply.github.com>
2017-12-11 15:29:11 +0200

    bootsrap and css to artistdisplay

--- client/src/components/Artistdisplay.js
+++ client/src/components/Artistdisplay.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getArtistAlbums } from '../spotify';
 import { store } from '../store';
 import { Link } from 'react-router-dom';
+import '../css/artistdisplay.css';
 
 export default class Artistdisplay extends Component {
 	constructor(props){
@@ -12,63 +13,67 @@ export default class Artistdisplay extends Component {
 
 	componentDidMount() {
 		this.setState({id: this.props.match.params.id, name: this.props.location.state.name});
-
 	}
 
 
   render(){
     return (
-			<div id="main-wrapper col-lg-12">
-      <div id="col-sm-3 col-md-6 col-lg-4">
-				{this.props.location.state.image.length > 0 &&
-					<img src={this.props.location.state.image[0].url} alt="artist"></img>
-				}
-				<p>{this.props.location.state.name}</p>
-				<h4>Genres</h4>
-				<div>
-				{this.props.location.state.genres.map(x =>
-					<p key={x} >{x}</p>
-				)}
+			<div className=" main-container container-fluid">
+      	<div className="main-wrapper row">
+					<div className="main-wrapper col-lg-3">
+						<div className="main-wrapper row">
+							<div className="component-wrapper col-lg-12">
+								<div className="artist-content">
+									{this.props.location.state.image.length > 0 &&
+										<img className="artistdisplay-artist-img" src={this.props.location.state.image[0].url} alt="artist"></img>
+									}
+									<h2>{this.props.location.state.name}</h2>
+										<div className="artist-genres-div">
+										<h3>Genres</h3>
+										{this.props.location.state.genres.map(x =>
+											<p key={x} >{x}</p>
+										)}
+										</div>
+      					</div>
+							</div>
+						</div>
+					</div>
+					<div className="component-wrapper col-lg-9">
+						{ store.getState().artistalbums.items &&
+						<div className="artist-content-albums">
+							<h2>Albums</h2>
+							<div className="ul-wrapper">
+							<ul className="artist-content-albums-ul">
+								{ store.getState().artistalbums.items.map((data) =>
+									<Link to={{
+										pathname: `/album/${data.id}`,
+						 					state: {
+												image: data.images[0].url,
+ 												artistname: data.artists[0].name,
+ 												artistid: data.artists[0].id,
+ 												albumname: data.name,
+ 												albumid: data.id
+						 					}
+ 				 					}} key={data.id}>
+										{
+											<li key={data.id}>
+											{ data.images.length > 0 &&
+												<div className="artistdisplay-album-div">
+													<img className="artistdisplay-album-img" src={data.images[0].url} alt={data.name} />
+													<p>{data.name}</p>
+												</div>
+											}
+											</li>
+										}
+								  </Link>
+								)}
+						  </ul>
+							</div>
+					  </div>
+					  }
+					</div>
 				</div>
-      </div>
-			<div>
-			{ store.getState().artistalbums.items &&
-			<div>
-				<ul>
-				<p>Albums</p>
-				{ store.getState().artistalbums.items.map((data) =>
-					<Link to={{
-						 pathname: `/album/${data.id}`,
-						 state: {
-							image: data.images[0].url,
- 							artistname: data.artists[0].name,
- 							artistid: data.artists[0].id,
- 							albumname: data.name,
- 							albumid: data.id
-						 }
- 				 	}} key={data.id}>
-					{
-						<li key={data.id}>
-
-										 { data.images.length > 0 &&
-											<div>
-												<img src={data.images[0].url} alt={data.name} />
-												<p>{data.name}</p>
-											</div>
-										 }
-
-						</li>
-					}
-					</Link>
-
-				)}
-				</ul>
-				</div>
-			}
-		</div>
-		</div>
-
-
+			</div>
     );
   }
 }
