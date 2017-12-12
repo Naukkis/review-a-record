6db7c793b9d69c29d3d1faa6ab78ee44d6dd0031
unknown <riku.pekis@gmail.com>
2017-12-12 13:16:38 +0200

    change home size

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,21 +6,9 @@ export default class Home extends Component {
     return (
       <div className="main-wrapper">
 		    <div className="container container-wrapper">
-			     <br></br>
-				   <br></br>
-				   <br></br>
-				   <br></br>
-           <br></br>
-           <br></br>
-				   <h1> Review App</h1>
-				   <br></br>
-				   <h3>Rewiew and listen to your favorite artists</h3>
-           <br></br>
-           <h2>Start by searching your favorite artist from the searchbar </h2>
-           <br></br>
-				   <br></br>
-				   <br></br>
-           <br></br>
+				  <h1> Review App</h1>
+				  <h3>Rewiew and listen to your favorite artists</h3>
+          <h2>Start by searching your favorite artist from the searchbar </h2>
 		</div>
 	</div>
     );
