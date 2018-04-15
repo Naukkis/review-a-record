14e081eb6e3799b588985bbdf8b711b4f808ca0d
Naukkis <22678066+Naukkis@users.noreply.github.com>
2018-04-15 12:24:39 +0300

    add pg-promise for monitoring db

--- server/db.js
+++ server/db.js
@@ -1,8 +1,11 @@
 var promise = require('bluebird');
+const monitor = require('pg-monitor');
 
 var options = {
   promiseLib: promise
 };
+
+monitor.attach(options, ['query', 'error']);
 
 var pgp = require('pg-promise')(options);
 var connectionString = process.env.DATABASE_URL;
